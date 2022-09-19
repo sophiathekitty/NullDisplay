de180407fa8df972639b72e7acd9011471f54192
sophia daniels <sophiathekitty@users.noreply.github.com>
2022-09-18 18:32:17 -0600

    nicer services handling and logging

--- services/every_minute.php
+++ services/every_minute.php
@@ -1,5 +1,8 @@
 <?php
-Settings::SaveSettingsVar("Services::SyncDisplays--start",date("H:i:s"));
+//Settings::SaveSettingsVar("Services::SyncDisplays--start",date("H:i:s"));
+Services::Start("NullDisplay::EveryMinute");
+Services::Log("NullDisplay::EveryMinute","SyncDisplays::Sync");
 SyncDisplays::Sync();
-Settings::SaveSettingsVar("Services::SyncDisplays--done",date("H:i:s"));
+Services::Complete("NullDisplay::EveryMinute");
+//Settings::SaveSettingsVar("Services::SyncDisplays--done",date("H:i:s"));
 ?>
